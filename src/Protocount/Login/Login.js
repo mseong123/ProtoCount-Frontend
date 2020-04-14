5de894de39c4f9cf37443140068188327ebb2554
mseong <mseong123@hotmail.com>
2020-04-14 09:58:12 +0800

    minor login change

--- src/Protocount/Login/Login.js
+++ src/Protocount/Login/Login.js
@@ -27,7 +27,7 @@ function Login(props) {
                 style={{top:0,left:0, zIndex:'100000',backgroundColor:'rgba(255,255,255,0.6'}}>
                     <div className='spinner-grow' style={{width:'4rem',height:'4rem'}}><span className='sr-only'></span></div>
                 </div>:null}
-                <div className='col-10 offset-1 col-md-4 offset-md-4'>
+                <div className='col-10 offset-1 col-md-6 offset-md-3 col-lg-4 offset-lg-4'>
                     <div className='jumbotron min-vh-100' style={{marginBottom:0}}>
                         <h1 className='my-5'>
                             Login
